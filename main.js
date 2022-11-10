392e16fdfa9f8af4766ce2348e7279f15757b530
Rafael Tavares <74204117+taiHen56@users.noreply.github.com>
2022-11-10 04:54:44 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ var splide = new Splide(".splide", {
 });
 
 splide.mount();
+
+function voirPlus(){
+  var ligne = document.getElementById("voir");
+  var btn = document.getElementById("btnVoir");
+
+  if (ligne.style.visibility === "collapse") {
+    ligne.style.visibility="visible";
+    btn.innerHTML="Montrer moins...";
+  } else {
+    ligne.style.visibility="collapse";
+    btn.innerHTML="Montrer plus...";
+
+  }
+}
+
+function ariane(){
+  var para1 = document.getElementById("cate");
+  var para2 = document.getElementById("actived");
+  var ariane = document.getElementById("courant");
+  para1.classList.add("actived");
+  para2.classList.remove("actived");
+  ariane.innerHTML=para1.innerHTML;
+
+  para1.onclick=ariane;
+}
+
